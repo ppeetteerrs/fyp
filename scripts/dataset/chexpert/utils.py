Changed WANDB to Tensorboard and fixed chexpert normalization

scripts/dataset/chexpert/utils.py
@@ -74,11 +74,8 @@ class CheXpertImg:
 
     def proc_img(self, size: int, base_dir: Path) -> np.ndarray:
         img = remove_border(self.img(base_dir))
-        img = crop(img, size=size)
-
-        if np.mean(img) < 50:
-            print(img, "has low brightness")
-        return img
+        img = crop(img, size=size).astype(np.float32)
+        return img / 255
 
     @classmethod
     def from_str(cls, string: str) -> "CheXpertImg":

scripts/psp/train.py
@@ -1,15 +1,14 @@
 """Train pSp"""
 
 import os
-from pathlib import Path
 from typing import Dict, Tuple, Union, cast
 
 import lpips
 import torch
 import torch.nn.functional as F
-import wandb
 from torch import Tensor
 from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 from tqdm import tqdm
 from utils import repeat, to_device
@@ -99,15 +98,9 @@ class Task:
         else:
             self.start_iter = 0
 
-        # Start wandb
-        wandb.init(
-            project="FYP",
-            entity="ppeetteerrs",
-            group="psp",
-            job_type="train",
-            name=OPTIONS.name,
-            config=OPTIONS.to_dict(),
-        )
+        # Start Tensorboard
+        self.writer = SummaryWriter(f"tb_logs/{OPTIONS.name}")
+        self.writer.add_text("Options", f"```yaml\n{OPTIONS.dumps_yaml()}\n```")
 
     def forward(self, imgs: Dict[str, Tensor]) -> Tuple[Dict[str, Tensor], Tensor]:
         """
@@ -159,6 +152,15 @@ class Task:
             loss_dict[f"lpips_{img}:{truth}"] = float(loss_lpips)
             loss += loss_lpips * weight
 
+        for img, truth, weight in TRAIN_OPTIONS.l1_spec:
+            truth_mask = imgs[truth] > imgs[truth].min()
+            loss_l1 = F.l1_loss(
+                imgs[img] * truth_mask,
+                imgs[truth] * truth_mask,
+            )
+            loss_dict[f"l1_{img}:{truth}"] = float(loss_l1)
+            loss += loss_l1 * weight
+
         # Accumulate loss
         loss_dict["loss"] = float(loss)
 
@@ -193,25 +195,23 @@ class Task:
             self.optimizer.step()
 
             # Logging
-            wandb.log(loss_dict, step=step)
+            for k, v in loss_dict.items():
+                self.writer.add_scalar(k, v, step)
 
             if step % TRAIN_OPTIONS.sample_interval == 0:
                 sample_imgs, _ = self.forward(to_device(next(self.sample_loader)))
                 key = "_".join(sample_imgs.keys())
                 img = torch.cat(list(sample_imgs.values()), dim=3)
 
-                wandb.log(
-                    {
-                        key: wandb.Image(
-                            make_grid(
-                                img,
-                                nrow=1,
-                                normalize=True,
-                                value_range=(-1, 1),
-                            )
-                        )
-                    },
-                    step=step,
+                self.writer.add_image(
+                    key,
+                    make_grid(
+                        img,
+                        nrow=1,
+                        normalize=True,
+                        value_range=(-1, 1),
+                    ),
+                    step,
                 )
 
             # Validation related

scripts/stylegan/generate.py
@@ -3,7 +3,6 @@ from os import environ
 from typing import cast
 
 import torch
-import wandb
 from stylegan2_torch import Generator
 from stylegan2_torch.utils import mixing_noise
 from torchvision.utils import make_grid, save_image
@@ -30,7 +29,10 @@ class Task:
         # Load checkpoint
         ckpt = torch.load(ARCH_OPTIONS.ckpt)
         self.g_ema.load_state_dict(ckpt["g_ema"])
-        self.mean_latent = self.g_ema.mean_latent(10000, "cuda")
+        if "latent_avg" in ckpt:
+            self.mean_latent = ckpt["latent_avg"].to("cuda")
+        else:
+            self.mean_latent = self.g_ema.mean_latent(10000, "cuda")
 
         # Make image output folders
         (OPTIONS.output_dir / "generated").mkdir(parents=True, exist_ok=True)

scripts/stylegan/train.py
@@ -1,5 +1,4 @@
 """Train StyleGAN"""
-from os import environ
 from typing import cast
 
 import torch
@@ -11,9 +10,9 @@ from stylegan2_torch.loss import g_loss as get_g_loss
 from stylegan2_torch.loss import g_reg_loss as get_g_reg_loss
 from stylegan2_torch.utils import mixing_noise
 from torch import distributed, optim
-from torch.backends.cudnn import benchmark
 from torch.nn.parallel import DistributedDataParallel
 from torch.utils.data import DataLoader, DistributedSampler
+from torch.utils.tensorboard import SummaryWriter
 from torchvision.utils import make_grid
 from tqdm import tqdm
 from utils import accumulate, repeat
@@ -51,12 +50,12 @@ class Task:
         self.g_optim = optim.Adam(
             generator.parameters(),
             lr=TRAIN_OPTIONS.lr * g_reg_ratio,
-            betas=(0, 0.99 ** g_reg_ratio),
+            betas=(0, 0.99**g_reg_ratio),
         )
         self.d_optim = optim.Adam(
             discriminator.parameters(),
             lr=TRAIN_OPTIONS.lr * d_reg_ratio,
-            betas=(0, 0.99 ** d_reg_ratio),
+            betas=(0, 0.99**d_reg_ratio),
         )
 
         # Load checkpoint
@@ -129,14 +128,8 @@ class Task:
                 dynamic_ncols=True,
                 smoothing=0.01,
             )
-            wandb.init(
-                project="FYP",
-                entity="ppeetteerrs",
-                group="stylegan",
-                job_type="train",
-                name=OPTIONS.name,
-                config=OPTIONS.to_dict(),
-            )
+            self.writer = SummaryWriter(f"tb_logs/{OPTIONS.name}")
+            self.writer.add_text("Options", f"```yaml\n{OPTIONS.dumps_yaml()}\n```")
         else:
             pbar = range(self.start_iter, TRAIN_OPTIONS.iterations + 1)
 
@@ -252,35 +245,39 @@ class Task:
             loss_reduced = reduce_loss_dict(loss_dict)
 
             if isinstance(pbar, tqdm):
-                wandb.log(
-                    {
-                        "d_loss": loss_reduced["d_loss"].mean().item(),
-                        "g_loss": loss_reduced["g_loss"].mean().item(),
-                        "d_reg_loss": loss_reduced["d_reg_loss"].mean().item(),
-                        "g_reg_loss": loss_reduced["g_reg_loss"].mean().item(),
-                        "real_score": loss_reduced["real_score"].mean().item(),
-                        "fake_score": loss_reduced["fake_score"].mean().item(),
-                        "mean_path": mean_path_length_avg,
-                    },
-                    step=step,
+                self.writer.add_scalar(
+                    "d_loss", loss_reduced["d_loss"].mean().item(), step
                 )
+                self.writer.add_scalar(
+                    "g_loss", loss_reduced["g_loss"].mean().item(), step
+                )
+                self.writer.add_scalar(
+                    "d_reg_loss", loss_reduced["d_reg_loss"].mean().item(), step
+                )
+                self.writer.add_scalar(
+                    "g_reg_loss", loss_reduced["g_reg_loss"].mean().item(), step
+                )
+                self.writer.add_scalar(
+                    "real_score", loss_reduced["real_score"].mean().item(), step
+                )
+                self.writer.add_scalar(
+                    "fake_score", loss_reduced["fake_score"].mean().item(), step
+                )
+                self.writer.add_scalar("mean_path", mean_path_length_avg, step)
 
                 if step % TRAIN_OPTIONS.sample_interval == 0:
                     with torch.no_grad():
                         self.g_ema.eval()
                         sample = self.g_ema([self.sample_z])
-                        wandb.log(
-                            {
-                                "sample": wandb.Image(
-                                    make_grid(
-                                        sample,
-                                        nrow=int(TRAIN_OPTIONS.sample_size ** 0.5),
-                                        normalize=True,
-                                        value_range=(-1, 1),
-                                    )
-                                )
-                            },
-                            step=step,
+                        self.writer.add_image(
+                            "sample",
+                            make_grid(
+                                sample,
+                                nrow=int(TRAIN_OPTIONS.sample_size**0.5),
+                                normalize=True,
+                                value_range=(-1, 1),
+                            ),
+                            step,
                         )
 
                 if step % TRAIN_OPTIONS.ckpt_interval == 0: